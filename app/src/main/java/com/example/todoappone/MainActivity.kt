4d7e5d986f1fab2b6a57f402d4f1f982fe4df4e6
zaid-kamil <xaidmetamorphos@gmail.com>
2023-01-10 15:11:46 +0530

    added the functionality to add content to database

--- app/src/main/java/com/example/todoappone/MainActivity.kt
+++ app/src/main/java/com/example/todoappone/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.example.todoappone
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.todoappone.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
 //        val navView: BottomNavigationView = binding.navView
 //
--- app/src/main/java/com/example/todoappone/ui/TaskFragment.kt
+++ app/src/main/java/com/example/todoappone/ui/TaskFragment.kt
@@ -1,12 +1,19 @@
 package com.example.todoappone.ui
 
+import android.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import com.example.todoappone.R
+import com.example.todoappone.TodoApplication
+import com.example.todoappone.data.Task
+import com.example.todoappone.databinding.FragmentTaskBinding
 
 class TaskFragment : Fragment() {
 
@@ -14,19 +21,42 @@ class TaskFragment : Fragment() {
         fun newInstance() = TaskFragment()
     }
 
-    private lateinit var viewModel: TaskViewModel
+    private lateinit var binding: FragmentTaskBinding
+    private val viewModel: TaskViewModel by activityViewModels {
+        InventoryViewModelFactory((activity?.application as TodoApplication).database.taskDao())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_task, container, false)
+    ): View {
+        binding = FragmentTaskBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabAdd.setOnClickListener { showAddTaskDialog(it) }
+    }
+
+    private fun showAddTaskDialog(it: View?) {
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle("Add Task")
+            .setView(R.layout.add_task_dialog_layout)
+            .setPositiveButton("Add") { dialog, _ ->
+                val taskName =
+                    (dialog as AlertDialog).findViewById<EditText>(R.id.editTitle)?.text.toString()
+                if(viewModel.addItem(title = taskName)){
+                    Toast.makeText(requireContext(), "Task Added", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+        dialog.show()
     }
 
 }
